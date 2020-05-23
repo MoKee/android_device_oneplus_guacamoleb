84503193ade824eb8b8266e32a486b179016d48d
Martincz Gao <martincz@mokeedev.com>
2020-05-23 15:37:04 +0800

    guacamoleb: mokee-ify

--- device.mk
+++ device.mk
@@ -12,10 +12,10 @@ $(call inherit-product, vendor/oneplus/guacamoleb/guacamoleb-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-mokee
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/frameworks/base/packages/SystemUI
+    $(LOCAL_PATH)/overlay-mokee/frameworks/base/packages/SystemUI
 
 # Audio
 PRODUCT_COPY_FILES += \
